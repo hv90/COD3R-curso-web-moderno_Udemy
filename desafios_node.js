6005339e84f3eef4835485d0364feab6d7cce19f
hv90 <hugomachado182@gmail.com>
2019-01-09 21:56:15 -0200

    desafio com filtro e reduce

--- desafios_node.js
+++ desafios_node.js
@@ -3,6 +3,14 @@ const axios = require ('axios')
 
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    console.log(funcionarios
+        .filter(chines)
+        .filter(femea)
+        .reduce(menorSalario))
 })
 
+const chines = funcionario => funcionario.pais == 'China'
+const femea = funcionario => funcionario.genero == 'F'
+const menorSalario = (funcionario1, funcionario2) => {
+    return funcionario1.salario < funcionario2.salario ? funcionario1 : funcionario2
+}
